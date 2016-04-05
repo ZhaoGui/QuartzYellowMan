3fa97215c16edc6e83a124412be28ccb227f8936
Gui Zhao <562953985@qq.com>
2016-04-05 09:14:20 +0800

    Initial Commit

---
+++ QuartzJelly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuartzJelly
+//
+//  Created by 赵贵 on 16/4/5.
+//  Copyright © 2016年 猎暄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuartzJelly/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QuartzJelly
+//
+//  Created by 赵贵 on 16/4/5.
+//  Copyright © 2016年 猎暄. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
